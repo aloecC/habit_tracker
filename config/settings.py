e66949703d209cc281423b3f21664eba0ed3d145
Дарья Щербакова <daraaloec29@gmail.com>
2026-03-03 14:01:50 +0300

    update project

--- config/settings.py
+++ config/settings.py
@@ -158,8 +158,11 @@ CACHES = {
     }
 }
 
-CELERY_BROKER_URL = 'redis://127.0.0.1:6379/0'
-CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379/1'
+
+CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL', 'redis://redis:6379/0')
+CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND', 'redis://redis:6379/1')
+
+
 CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
 
 CELERY_ACCEPT_CONTENT = ['json']
